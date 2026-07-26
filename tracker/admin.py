0d9d1aa5a60d9efbf9205085cd251bfa3652160a
Emma Norling <norling@acm.org>
2026-07-26 16:35:40 +0100

    If only one bread machine, this is the default

--- tracker/admin.py
+++ tracker/admin.py
@@ -9,3 +9,13 @@ class BreadMachineAdmin(admin.ModelAdmin):
 class LoafAdmin(admin.ModelAdmin):
     list_display = ('bread_type', 'machine', 'started_at', 'ready_at', 'is_active')
     list_filter = ('machine',)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        # Check if Django is loading the dropdown for the 'machine' field
+        if db_field.name == "machine":
+            machines = BreadMachine.objects.all()
+            # If there is exactly 1 machine, set it as the default initial value
+            if machines.count() == 1:
+                kwargs['initial'] = machines.first().id
+                
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
