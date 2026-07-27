85a2494d6bd13a9f9e8f18f0fe5a6f440a28867a
Emma Norling <norling@acm.org>
2026-07-27 17:14:03 +0100

    tweaks

--- bread_tracker/urls.py
+++ bread_tracker/urls.py
@@ -15,12 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
 from tracker import views
 from tracker.views import public_dashboard
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', public_dashboard, name='dashboard'),
     path('loaf/<int:loaf_id>/', views.loaf_detail, name='loaf_detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
